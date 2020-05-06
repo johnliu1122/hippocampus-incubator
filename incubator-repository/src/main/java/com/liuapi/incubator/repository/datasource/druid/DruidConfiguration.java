7428332a8233d4f7f4571164e83bf59bbbe87831
johnliu1122 <johnliu1122@163.com>
2020-05-07 01:48:51 +0800

    [存储]添加druid相关的配置

---
+++ incubator-repository/src/main/java/com/liuapi/incubator/repository/datasource/druid/DruidConfiguration.java
@@ -0,0 +1,30 @@
+package com.liuapi.incubator.repository.datasource.druid;
+
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.google.common.collect.Lists;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DruidConfiguration {
+    @Primary
+    @Bean
+    @ConfigurationProperties("spring.datasource.shard1.druid")
+    public DataSource shard1() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties("spring.datasource.shard2.druid")
+    public DataSource shard2() {
+        return DruidDataSourceBuilder.create().build();
+    }
+}
